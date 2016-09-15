589a3b979730686568db0bf9d3173dc7887ad526
Will Sweet <william.sweet2@gmail.com>
2016-09-15 09:52:44 +0100

    Offset the fade in (mobile fix)

--- script.js
+++ script.js
@@ -21,17 +21,18 @@ $(document).ready(function(){
 
 	/* <<<<<<<< Fade elements in on scroll >>>>>>>> */
 
-	 /* Every time the window is scrolled ... */
+    /* Every time the window is scrolled ... */
     $(window).scroll( function(){
     
         /* Check the location of each desired element */
         $('.hideme').each( function(i){
             
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            var mobileOffset = 100;
+            var bottomOfObject = $(this).offset().top + $(this).outerHeight();
+            var bottomOfWindow = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
+            if( bottomOfWindow + mobileOffset > bottomOfObject ){
                 
                 $(this).animate({'opacity':'1'},500);
                     
@@ -40,6 +41,7 @@ $(document).ready(function(){
         }); 
     
     });
+
     /* <<<<<<<< END Fade elements in on scroll >>>>>>>> */
 
 	/* <<<<<<<< Images >>>>>>>> */
